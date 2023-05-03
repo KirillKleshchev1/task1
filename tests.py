7a05cb735d48d87a1a525bf8b003fafc69bffa0d
Kirill.Kleshchev1 <theobject705@gmail.com>
2023-05-03 20:21:11 +0500

    Исправление тестов

--- tests.py
+++ tests.py
@@ -6,52 +6,52 @@ import main
 class TestTest(unittest.TestCase):
     def test_kmp(self):
         pars = algorithms.Parser('aba', 'a')
-        self.assertEqual(main.TYPE['kmp'](pars), [0, 2])
+        assert main.TYPE['kmp'](pars) == [0, 2]
 
     def test_kmp_not_pattern(self):
         pars = algorithms.Parser('aba', 'c')
-        self.assertEqual(main.TYPE['kmp'](pars), [])
+        assert main.TYPE['kmp'](pars) == []
 
     def test_kmp_pattern_longer_text(self):
         pars = algorithms.Parser('a', 'aba')
-        self.assertEqual(main.TYPE['kmp'](pars), [])
+        assert main.TYPE['kmp'](pars) == []
 
     def test_boyer_moore(self):
         pars = algorithms.Parser('aba', 'a')
-        self.assertEqual(main.TYPE['boyer_moore'](pars), [0, 2])
+        assert main.TYPE['boyer_moore'](pars) == [0, 2]
 
     def test_boyer_moore_not_pattern(self):
         pars = algorithms.Parser('aba', 'c')
-        self.assertEqual(main.TYPE['boyer_moore'](pars), [])
+        assert main.TYPE['boyer_moore'](pars) == []
 
     def test_boyer_moore_pattern_longer_text(self):
         pars = algorithms.Parser('c', 'aba')
-        self.assertEqual(main.TYPE['boyer_moore'](pars), [])
+        assert main.TYPE['boyer_moore'](pars) == []
 
     def test_rk(self):
         pars = algorithms.Parser('aba', 'a')
-        self.assertEqual(main.TYPE['rabin_karp'](pars), [0, 2])
+        assert main.TYPE['rabin_karp'](pars) == [0, 2]
 
     def test_rk_not_pattern(self):
         pars = algorithms.Parser('aba', 'c')
-        self.assertEqual(main.TYPE['rabin_karp'](pars), [])
+        assert main.TYPE['rabin_karp'](pars) == []
 
     def test_rk_pattern_longer_text(self):
         pars = algorithms.Parser('c', 'aba')
-        self.assertEqual(main.TYPE['rabin_karp'](pars), [])
+        assert main.TYPE['rabin_karp'](pars) == []
 
     def test_native(self):
         pars = algorithms.Parser('aba', 'a')
-        self.assertEqual(main.TYPE['naive'](pars), [0, 2])
+        assert main.TYPE['naive'](pars) == [0, 2]
 
     def test_naive_not_pattern(self):
         pars = algorithms.Parser('aba', 'c')
-        self.assertEqual(main.TYPE['naive'](pars), [])
+        assert main.TYPE['naive'](pars) == []
 
     def test_naive_pattern_longer_text(self):
         pars = algorithms.Parser('c', 'aba')
-        self.assertEqual(main.TYPE['naive'](pars), [])
+        assert main.TYPE['naive'](pars) == []
 
     def test_types_mode(self):
         pars = algorithms.Parser('aba', 'a')
-        self.assertEqual(main.TYPES_MODE['time_check'](pars, main.TYPE['kmp']), 0.0)
+        assert main.TYPES_MODE['time_check'](pars, main.TYPE['kmp']) == 0.0
